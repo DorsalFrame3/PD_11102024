b76bf033a807c4352adec1616a7c0999a7807413
Your Name <you@example.com>
2024-10-11 10:19:50 +0300

    debugging

--- laravel-api-backend/app/Models/User.php
+++ laravel-api-backend/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -46,7 +47,8 @@ class User extends Authenticatable
         ];
     }
 
-    public function posts() {
+    public function posts() : HasMany
+    {
         return $this->hasMany(Post::class);
     }
 }
